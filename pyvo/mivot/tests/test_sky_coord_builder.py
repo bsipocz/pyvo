f7a6d54f47d472dbf6da4cc931228e8cefd27b0f
lmichel <laurent.michel@astro.unistra.fr>
2025-10-07 15:36:33 +0200

    stylecheck

--- pyvo/mivot/tests/test_sky_coord_builder.py
+++ pyvo/mivot/tests/test_sky_coord_builder.py
@@ -235,7 +235,6 @@ def test_vizier_output_with_equinox_and_parallax():
             == "<SkyCoord (FK5: equinox=J2012.000): (ra, dec, distance) in "
                "(deg, deg, pc)(52.26722684, 59.94033461, 1666.66666667) "
                "(pm_ra_cosdec, pm_dec) in mas / yr(-0.82, -1.85)>")
-    
 
     mydict = deepcopy(vizier_equin_dict)
     mydict["spaceSys"]["frame"]["spaceRefFrame"]["value"] = "FK4"
